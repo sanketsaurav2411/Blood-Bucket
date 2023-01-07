068b32fbe2ca0d45b19c9cfb204d24faf82d3c6f
Sanket Saurav <sanket.sam01@gmail.com>
2023-01-08 00:00:28 +0530

    Commit files

---
+++ donor/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Donor(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    bloodgroup = models.CharField(max_length=10)
+    address = models.CharField(max_length=40)
+    mobile = models.CharField(max_length=20,null=False)
+   
+    @property
+    def get_name(self):
+        return self.user.first_name+" "+self.user.last_name
+    @property
+    def get_instance(self):
+        return self
+    def __str__(self):
+        return self.user.first_name
+
+class BloodDonate(models.Model): 
+    donor = models.ForeignKey(Donor,on_delete=models.CASCADE)   
+    disease = models.CharField(max_length=100,default="Nothing")
+    age = models.PositiveIntegerField()
+    bloodgroup = models.CharField(max_length=10)
+    unit = models.PositiveIntegerField(default=0)
+    status = models.CharField(max_length=20,default="Pending")
+    date = models.DateField(auto_now=True)
+    def __str__(self):
+        return self.donor
